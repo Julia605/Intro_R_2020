1cd4cc038ca7406447c56605106c719b13df829d
Julia605 <thibelinthabiseng25@gmail.com>
2020-01-28 18:31:27 +0200

    exercise 4.5

---
+++ exercise.R
@@ -0,0 +1,24 @@
+# Day 1
+# Data laminaria data analyses
+# 28 January 2020
+
+# loading packages
+library(tidyverse)
+
+laminaria <- read_csv("data/laminaria.csv")
+
+mutate(total_length_half= total_length/2)%>%
+
+  select(site,total_length_half(na.rm=TRUE))  
+
+group_by(site,blade_length)%>%
+  summarise (min_blade_length=min(blade_length),
+            max_blade_length=max(blade_length),
+            mean_blade_length=mean(blade_length))
+ laminaria%>%
+  group_by(site)%>% 
+  summarise (max_stipe_mass=max(stipe_mass))
+
+laminaria%>%
+  select(site,region,stipe_length)
+  
